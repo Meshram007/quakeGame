6359ea365522082da9e0ad13528db292da5a4483
Meshram007 <nikhil0299@gmail.com>
2023-07-28 00:21:06 +0530

    func: to read the logs

--- main.go
+++ main.go
@@ -19,4 +19,20 @@ type GameStats struct {
 	KillsByMeans map[string]int `json:"kills_by_means"` // Number of kills by means (weapon).
 }
 
+// readLogFile reads the log file and returns the content as a slice of strings (lines).
+func readLogFile(filename string) []string {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines
+}
+
 
